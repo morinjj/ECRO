b620df1802d304adcc4b6c96a8d81b8c7591a7b7
morinjj <113720584+morinjj@users.noreply.github.com>
2024-12-05 01:15:37 +0000

    second test

--- firebase.js
+++ firebase.js
@@ -1,49 +1,35 @@
 // backend/firebase.jsw
 import { initializeApp } from "firebase/app";
-import {
-    getFirestore,
-    collection,
-    getDocs,
-    getDoc,
-    addDoc,
-    doc,
-    updateDoc,
-    deleteDoc,
-    setDoc,
-    query,
-    where,
-} from "firebase/firestore";
+import * as firestore_1 from "firebase/firestore";
 
-const firebaseConfig = {
+
+// Initialize Firebase
+initializeApp({
     apiKey: "AIzaSyB6GQNrbnJtCVlews_IQhuKsWknxmX11n4",
     authDomain: "uecro-wyyugl.firebaseapp.com",
     projectId: "uecro-wyyugl",
     storageBucket: "uecro-wyyugl.firebasestorage.app",
     messagingSenderId: "763018091638",
     appId: "1:763018091638:web:e53dbe2aa8645ed0035357"
-};
-
-// Initialize Firebase
-initializeApp(firebaseConfig);
+});
 
 // Get Firestore instance
-const firestore = getFirestore();
+const firestore = firestore_1.getFirestore();
 
 // Function to get documents from a collection
 export async function firestoreGetDocuments(collectionName, filter) {
     try {
         let q;
         if (filter) {
-            q = query(collection(firestore, collectionName), where(filter.field, filter.operator, filter.value));
+            q = firestore_1.query(firestore_1.collection(firestore, collectionName), firestore_1.where(filter.field, filter.operator, filter.value));
         } else {
-            q = collection(firestore, collectionName);
+            q = firestore_1.collection(firestore, collectionName);
         }
-        const querySnapshot = await getDocs(q);
-        const documents = [];
+        const querySnapshot = await firestore_1.getDocs(q);
         querySnapshot.forEach((doc) => {
-            documents.push({ _id: doc.id, ...doc.data() });
+            [].push({ _id: doc.id, ...doc.data() });
         });
-        return documents;
+        return [];
     } catch (error) {
         console.error("Error getting documents:", error);
         throw error;
@@ -53,8 +39,8 @@ export async function firestoreGetDocuments(collectionName, filter) {
 // Function to get a single document
 export async function firestoreGetDocument(collectionName, docId) {
     try {
-        const docRef = doc(firestore, collectionName, docId);
-        const docSnap = await getDoc(docRef);
+        const docRef = firestore_1.doc(firestore, collectionName, docId);
+        const docSnap = await firestore_1.getDoc(docRef);
         if (docSnap.exists()) {
             return docSnap.data();
         } else {
@@ -72,10 +58,10 @@ export async function firestoreCreateDocument(collectionName, data, docId = null
     try {
         let docRef;
         if (docId) {
-            docRef = doc(firestore, collectionName, docId);
-            await setDoc(docRef, data);
+            docRef = firestore_1.doc(firestore, collectionName, docId);
+            await firestore_1.setDoc(docRef, data);
         } else {
-            docRef = await addDoc(collection(firestore, collectionName), data);
+            docRef = await firestore_1.addDoc(firestore_1.collection(firestore, collectionName), data);
         }
         console.log("Document written with ID:", docRef.id);
         return docRef.id;
@@ -88,8 +74,8 @@ export async function firestoreCreateDocument(collectionName, data, docId = null
 // Function to update an existing document
 export async function firestoreUpdateDocument(collectionName, docId, data) {
     try {
-        const docRef = doc(firestore, collectionName, docId);
-        await updateDoc(docRef, data);
+        const docRef = firestore_1.doc(firestore, collectionName, docId);
+        await firestore_1.updateDoc(docRef, data);
         console.log("Document updated with ID:", docId);
     } catch (error) {
         console.error("Error updating document:", error);
@@ -100,7 +86,7 @@ export async function firestoreUpdateDocument(collectionName, docId, data) {
 // Function to delete a document
 export async function firestoreDeleteDocument(collectionName, docId) {
     try {
-        await deleteDoc(doc(firestore, collectionName, docId));
+        await firestore_1.deleteDoc(firestore_1.doc(firestore, collectionName, docId));
         console.log("Document deleted with ID:", docId);
     } catch (error) {
         console.error("Error deleting document:", error);
